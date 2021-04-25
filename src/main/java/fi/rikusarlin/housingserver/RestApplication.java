e15a3ca4a7bbacd4003d4efd3559753b6cf53899
Riku Sarlin <riku.sarlin@gmail.com>
2021-04-25 22:26:31 +0300

    Fixed openapi generation

--- src/main/java/fi/rikusarlin/housingserver/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package fi.rikusarlin.housingserver;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api/v2/housing")
-public class RestApplication extends Application {
-
-}
--- src/main/java/fi/rikusarlin/housingserver/model/ExpenseType.java
+++
@@ -1,9 +0,0 @@
-package fi.rikusarlin.housingserver.model;
-
-import java.util.Objects;
-import java.util.ArrayList;
-import com.fasterxml.jackson.annotation.JsonValue;
-import javax.validation.constraints.*;
-public enum ExpenseType {
-    RENT, ELECTRICITY, WATER, INTEREST, MAINTENANCE_CHARGE, OTHER
-}
